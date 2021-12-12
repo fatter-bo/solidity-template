747abb6e0b4fce6461e677d52eeb512db2d57df7
test <test@test.test>
2021-12-12 12:52:00 +0800

    .gitignore

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -12,6 +12,16 @@ import { config as dotenvConfig } from "dotenv";
 import { HardhatUserConfig } from "hardhat/config";
 import { NetworkUserConfig } from "hardhat/types";
 
+import { task } from "hardhat/config";
+
+task("accounts", "Prints the list of accounts", async (args, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.info("account:", account.address);
+  }
+});
+
 dotenvConfig({ path: resolve(__dirname, "./.env") });
 
 const chainIds = {
@@ -66,32 +76,127 @@ const config: HardhatUserConfig = {
     kovan: getChainConfig("kovan"),
     rinkeby: getChainConfig("rinkeby"),
     ropsten: getChainConfig("ropsten"),
-  },
-  paths: {
-    artifacts: "./artifacts",
-    cache: "./cache",
-    sources: "./contracts",
-    tests: "./test",
-  },
-  solidity: {
-    version: "0.8.9",
-    settings: {
-      metadata: {
-        // Not including the metadata hash
-        // https://github.com/paulrberg/solidity-template/issues/31
-        bytecodeHash: "none",
+    mainnet: {
+      commit_url: "http://10.0.0.89:18545",
+      commit_ws: "ws://10.0.0.89:18546",
+      url: "http://10.0.0.89:18545",
+      ws: "ws://10.0.0.51:31004",
+      slavers: [
+        "ws://10.0.0.51:31004",
+        "ws://10.0.0.89:30004",
+        "ws://10.0.0.89:18546",
+        "wss://mainnet.infura.io/ws/v3/" + infuraApiKey,
+      ],
+      accounts: {
+        mnemonic,
       },
-      // Disable the optimizer when debugging
-      // https://hardhat.org/hardhat-network/#solidity-optimizer-support
-      optimizer: {
-        enabled: true,
-        runs: 800,
+      chainId: 1,
+    },
+    bsctestnet: {
+      //url: "https://data-seed-prebsc-1-s1.binance.org:8545",
+      //url: "https://data-seed-prebsc-2-s1.binance.org:8545",
+      url: "https://data-seed-prebsc-2-s3.binance.org:8545",
+      gasPrice: 20000000000,
+      rpcUr: "https://data-seed-prebsc-2-s3.binance.org:8545",
+      blockUrl: "https://testnet.bscscan.com/",
+      allowUnlimitedContractSize: true,
+      accounts: {
+        mnemonic,
       },
+      chainId: 97,
+    },
+    bsc: {
+      url: "https://dataseed1.binance.org/",
+      blockUrl: "https://bscscan.com/",
+      gasPrice: 20000000000,
+      accounts: {
+        mnemonic,
+      },
+      chainId: 56,
+    },
+    ganache: {
+      url: "http://127.0.0.1:7545",
+      ws: "ws://127.0.0.1:7546",
+      allowUnlimitedContractSize: true,
+      chainId: 1337,
+    },
+    localhost: {
+      url: "http://127.0.0.1:8545",
+      ws: "ws://127.0.0.1:8546",
+      allowUnlimitedContractSize: true,
+      chainId: chainIds.hardhat,
     },
   },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./build/cache",
+    artifacts: "./build/artifacts",
+  },
   typechain: {
-    outDir: "src/types",
+    //outDir: "src/types",
     target: "ethers-v5",
+  },
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.9",
+        settings: {
+          metadata: {
+            // Not including the metadata hash
+            // https://github.com/paulrberg/solidity-template/issues/31
+            bytecodeHash: "none",
+          },
+          // Disable the optimizer when debugging
+          // https://hardhat.org/hardhat-network/#solidity-optimizer-support
+          optimizer: {
+            enabled: true,
+            runs: 800,
+          },
+        },
+      },
+      {
+        version: "0.8.6",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 1000000,
+          },
+        },
+      },
+      {
+        version: "0.8.3",
+      },
+      {
+        version: "0.5.5",
+      },
+      {
+        version: "0.6.7",
+        settings: {},
+      },
+      {
+        version: "0.7.4",
+      },
+      {
+        version: "0.8.4",
+      },
+      {
+        version: "0.4.23",
+      },
+    ],
+  },
+  gasReporter: {
+    currency: "USD",
+    enabled: true,
+  },
+  etherscan: {
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
+    apiKey: "<api-key>",
+  },
+  tenderly: {
+    project: process.env.TENDERLY_PROJECT || "hardcatstudy",
+    username: process.env.TENDERLY_USERNAME || "fatter",
   },
 };
 
